1e2ae29384a22b691b0e243fc3296f56da58d1da
philopian <pdotwdot@gmail.com>
2017-11-14 13:50:22 -0800

    appseed start --dojo now available

---
+++ config/webpack.config.dojo.js
@@ -0,0 +1,114 @@
+process.env.NODE_ENV = "development";
+const path = require("path");
+const webpack = require("webpack");
+const config = require("../config");
+const autoprefixer = require("autoprefixer");
+
+module.exports = {
+  // GENERAL STUFF
+  target: "web",
+  devtool: "cheap-module-eval-source-map",
+  plugins: [
+    // new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': `"development"`
+      }
+    })
+  ],
+  resolve: {
+    modules: ["node_modules", "bower_components"],
+    descriptionFiles: ["package.json", "bower.json"],
+    alias: {
+      bower_components: "../../bower_components"
+    }
+  },
+  externals: /^esri/,
+
+  // IN/OUT files
+  entry: [
+    require.resolve("babel-polyfill"),
+    require.resolve("react-dev-utils/webpackHotDevClient"),
+    config.paths.webRoot
+  ],
+  output: {
+    path: config.paths.webRoot,
+    publicPath: "/",
+    filename: "bundle.js",
+    library: "app/bundle",
+    libraryTarget: "amd"
+  },
+
+  // LOADERS
+  module: {
+    rules: [
+      { test: /\.html$/, use: [{ loader: require.resolve("html-loader"), options: { minimize: true } }] },
+      {
+        test: /\.js$/,
+        include: config.paths.webRoot,
+        loader: require.resolve("babel-loader"),
+        options: {
+          babelrc: false,
+          presets: [require("babel-preset-airbnb"), require("babel-preset-env")]
+        }
+      },
+      {
+        test: /\.jsx$/,
+        include: config.paths.webRoot,
+        loader: require.resolve("babel-loader"),
+        options: {
+          babelrc: false,
+          presets: [
+            require("babel-preset-airbnb"),
+            require("babel-preset-env"),
+            require("babel-preset-react")
+          ]
+        }
+      },
+      { test: /\.css$/, loaders: [require("style-loader"), require("css-loader")] },
+      {
+        test: /\.scss$/,
+        include: config.paths.webRoot,
+        use: [
+          require.resolve("style-loader"),
+          {
+            loader: require.resolve("css-loader"),
+            options: {}
+          },
+          {
+            loader: require.resolve("postcss-loader"),
+            options: {
+              // Necessary for external CSS imports to work
+              // https://github.com/facebookincubator/create-react-app/issues/2677
+              ident: "postcss",
+              plugins: () => [
+                require("postcss-flexbugs-fixes"),
+                autoprefixer({
+                  browsers: [
+                    ">1%",
+                    "last 4 versions",
+                    "Firefox ESR",
+                    "not ie < 9" // React doesn't support IE8 anyway
+                  ],
+                  flexbox: "no-2009"
+                })
+              ]
+            }
+          },
+          {
+            loader: require.resolve("sass-loader"),
+            options: {}
+          }
+        ]
+      },
+      {
+        exclude: [/\.js$/, /\.jsx$/, /\.html$/, /\.json$/, /\.scss$/, /\.css$/, /\.ttf$/],
+        loader: require.resolve("file-loader"),
+        options: {
+          name: "static/media/[name].[hash:8].[ext]"
+        }
+      }
+    ]
+  }
+};
--- scripts/start.js
+++ scripts/start.js
@@ -4,6 +4,7 @@ const path = require("path");
 const argv = process.argv.slice(2);
 const chalk = require("chalk");
 const shell = require("shelljs");
+const _ = require("lodash");
 
 const appDirectory = fs.realpathSync(process.cwd());
 const config = require(path.join(appDirectory, "appseed.config.js"));
@@ -11,8 +12,8 @@ console.log(
   "",
   chalk.bgCyan("Command:"),
   " $ appseed start\n",
-  chalk.bgCyan("Application Root:"),
-  ` ${appDirectory}\n`,
+  // chalk.bgCyan("Application Root:"),
+  // ` ${appDirectory}\n`,
   chalk.bgCyan("Argument:"),
   ` ${argv}\n`
 );
@@ -27,10 +28,19 @@ bowerTags.injectTagsIntoHtml(appDirectory);
 bowerTags.watch(appDirectory);
 //*/
 
+
+
 // Run Webpack
 const Webpack = require("webpack");
 const WebpackDevServer = require("webpack-dev-server");
-const webpackConfig = require("../config/webpack.config.dev.js");
+
+// User provided the "--dojo" argument
+let webpackConfig;
+if (_.includes(argv, "--dojo")) {
+  webpackConfig = require("../config/webpack.config.dojo.js");
+} else {
+  webpackConfig = require("../config/webpack.config.dev.js");
+}
 const openBrowser = require("react-dev-utils/openBrowser");
 const compiler = Webpack(webpackConfig);
 var server = new WebpackDevServer(compiler, {
