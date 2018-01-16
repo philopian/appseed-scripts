914b2182962d2bf82780ccc90300b6359a1f648f
philopian <pdotwdot@gmail.com>
2018-01-15 17:41:48 -0800

    added stage-2 to jest

--- config/jest/preprocessor.js
+++ config/jest/preprocessor.js
@@ -1,12 +1,12 @@
-const babel = require('babel-core')
-const jestPreset = require('babel-preset-jest')
+const babel = require("babel-core");
+const jestPreset = require("babel-preset-jest");
 
 module.exports = {
   process: function(src) {
     const transformCfg = {
-      presets: ['react', 'env', jestPreset],
+      presets: ["react", "env", "stage-2", jestPreset],
       retainLines: true
-    }
-    return babel.transform(src, transformCfg).code
+    };
+    return babel.transform(src, transformCfg).code;
   }
-}
+};
