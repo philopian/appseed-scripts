0b33cd934c684c314c94c5459e9fb9854db1edc6
philopian <pdotwdot@gmail.com>
2018-07-10 14:49:19 -0700

    upgraded to webpack 4

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,24 +1,33 @@
 process.env.NODE_ENV = 'development';
 const path = require('path');
 const webpack = require('webpack');
+// const config = require("./appseed.config");
 const config = require("../config");
-const autoprefixer = require('autoprefixer');
+
+
+
+console.log('---PORT', process.env.PORT  ,config.port);
+
+
 
 module.exports = {
-  // GENERAL STUFF
-  target: 'web',
-  devtool: 'cheap-module-eval-source-map',
-  plugins: [
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': `"development"`
-      }
-    })
+  mode: "development",
+  
+  // IN/OUT files
+  entry: [
+    require.resolve('babel-polyfill'),
+    require.resolve('react-dev-utils/webpackHotDevClient'),
+    config.paths.webRoot
   ],
+  output: {
+    publicPath: '/',
+    path: config.paths.appRoot,
+    publicPath: 'www/',
+    filename: 'bundle.js'
+  },
+
+  // GENERAL STUFF
   resolve: {
-    extensions: ['.js', '.jsx'],
     modules: ['node_modules', 'bower_components'],
     descriptionFiles: ['package.json', 'bower.json'],
     alias: {
@@ -27,122 +36,208 @@ module.exports = {
       AppRoot: path.resolve(config.paths.appRoot)
     }
   },
-  node: {
-    fs: 'empty'
-  },
-
-
-  // IN/OUT files
-  entry: [
-    require.resolve('babel-polyfill'),
-    require.resolve('react-dev-utils/webpackHotDevClient'),
-    config.paths.webRoot
+  devtool: 'cheap-module-eval-source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
   ],
-  output: {
-    path: config.paths.appRoot,
-    publicPath: 'www/',
-    filename: 'bundle.js'
-  },
-
 
   // LOADERS
   module: {
-    rules: [ //
-      {
-        test: /\.html$/,
-        use: [{
-          loader: require.resolve("html-loader"),
-          options: { minimize: true }
-        }]
-      },
-      {
-        test: /\.(js|jsx)$/,
-        include: config.paths.webRoot,
-        loader: require.resolve("babel-loader"),
-        options: {
-          babelrc: false,
-          presets: [
-            require("babel-preset-env"),
-            require("babel-preset-react"),
-            require("babel-preset-stage-2"),
-          ]
-        }
-      },
-      {
-        test: /\.css$/,
-        // include: config.paths.appRoot,
-        use: [
-          require.resolve("style-loader"),
-          {
-            loader: require.resolve("css-loader"),
-            options: {}
-          },
-          {
-            loader: require.resolve("postcss-loader"),
-            options: {
-              ident: "postcss",
-              plugins: () => [
-                require("postcss-flexbugs-fixes"),
-                autoprefixer({
-                  browsers: [
-                    ">1%",
-                    "last 4 versions",
-                    "Firefox ESR",
-                    "not ie < 9" // React doesn't support IE8 anyway
-                  ],
-                  flexbox: "no-2009"
-                })
-              ]
-            }
-          }
-        ]
-      },
-      {
-        test: /\.scss$/,
-        // include: config.paths.appRoot,
-        use: [
-          require.resolve("style-loader"),
-          {
-            loader: require.resolve("css-loader"),
-            options: {}
-          },
-          {
-            loader: require.resolve("postcss-loader"),
-            options: {
-              ident: "postcss",
-              plugins: () => [
-                require("postcss-flexbugs-fixes"),
-                autoprefixer({
-                  browsers: [
-                    ">1%",
-                    "last 4 versions",
-                    "Firefox ESR",
-                    "not ie < 9" // React doesn't support IE8 anyway
-                  ],
-                  flexbox: "no-2009"
-                })
-              ]
-            }
-          },
-          {
-            loader: require.resolve("sass-loader"),
-            options: {}
-          }
-        ]
-      },
-
-      {
-        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: 'file-loader?name=fonts/[name].[ext]'
-      },
-
-      {
-        exclude: [/\.js$/, /\.jsx$/, /\.html$/, /\.json$/, /\.scss$/, /\.css$/, /\.ttf$/],
-        loader: require.resolve('file-loader'),
-        options: {
-          name: 'static/media/[name].[hash:8].[ext]',
-        },
-      }
+    rules: [
+      { test: /\.html$/, use: [{ loader: require.resolve('html-loader'), options: { minimize: true }, }], },
+      { test: /\.(css|scss)$/, use: [require.resolve('style-loader'), require.resolve('css-loader'),require.resolve('sass-loader')], },
+      { test: /\.(js|jsx)$/,include: config.fileNames.webRoot,loader: require.resolve("babel-loader"),options: {babelrc: false,presets: [require("babel-preset-env"),require("babel-preset-react"),require("babel-preset-stage-0")]}},
+      { test: /\.(jpg|jpeg|png|svg|gif)$/, loader: 'file-loader?name=[path][name].[ext]' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: [{ loader: require.resolve('file-loader') }] },
+      { test: /\.(woff|woff2)$/, use: [{ loader: 'url-loader?prefix=font/&limit=5000' }] },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, use: [{ loader: 'url-loader?limit=10000&mimetype=application/octet-stream' }] },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: [{ loader: 'url-loader?limit=10000&mimetype=image/svg+xml' }] }
     ]
+  },
+
+  // WEBPACK-DEV-SERVER
+  devServer: {
+    contentBase: path.join(__dirname, 'www'),
+    hot: true,
+    inline: true,
+    quiet: true,
+    port: process.env.PORT  || config.port,
+    historyApiFallback: {
+      index: '/index.html'
+    }
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// process.env.NODE_ENV = 'development';
+// const path = require('path');
+// const webpack = require('webpack');
+// const config = require("../config");
+// const autoprefixer = require('autoprefixer');
+
+// module.exports = {
+//   // GENERAL STUFF
+//   // target: 'web',
+//   // devtool: 'cheap-module-eval-source-map',
+//   // plugins: [
+//   //   new webpack.NamedModulesPlugin(),
+//   //   new webpack.HotModuleReplacementPlugin(),
+//   //   new webpack.DefinePlugin({
+//   //     'process.env': {
+//   //       'NODE_ENV': `"development"`
+//   //     }
+//   //   })
+//   // ],
+//   // resolve: {
+//   //   extensions: ['.js', '.jsx'],
+//   //   modules: ['node_modules', 'bower_components'],
+//   //   descriptionFiles: ['package.json', 'bower.json'],
+//   //   alias: {
+//   //     BowerComponents: path.resolve(config.paths.appRoot + "/bower_components"),
+//   //     NodeModules: path.resolve(config.paths.appRoot + "/node_modules"),
+//   //     AppRoot: path.resolve(config.paths.appRoot)
+//   //   }
+//   // },
+//   // node: {
+//   //   fs: 'empty'
+//   // },
+
+
+//   // // IN/OUT files
+//   // entry: [
+//   //   require.resolve('babel-polyfill'),
+//   //   require.resolve('react-dev-utils/webpackHotDevClient'),
+//   //   config.paths.webRoot
+//   // ],
+//   // output: {
+//   //   path: config.paths.appRoot,
+//   //   publicPath: 'www/',
+//   //   filename: 'bundle.js'
+//   // },
+
+
+//   // LOADERS
+//   module: {
+//     rules: [ //
+//       {
+//         test: /\.html$/,
+//         use: [{
+//           loader: require.resolve("html-loader"),
+//           options: { minimize: true }
+//         }]
+//       },
+//       {
+//         test: /\.(js|jsx)$/,
+//         include: config.paths.webRoot,
+//         loader: require.resolve("babel-loader"),
+//         options: {
+//           babelrc: false,
+//           presets: [
+//             require("babel-preset-env"),
+//             require("babel-preset-react"),
+//             require("babel-preset-stage-2"),
+//           ]
+//         }
+//       },
+//       {
+//         test: /\.css$/,
+//         // include: config.paths.appRoot,
+//         use: [
+//           require.resolve("style-loader"),
+//           {
+//             loader: require.resolve("css-loader"),
+//             options: {}
+//           },
+//           {
+//             loader: require.resolve("postcss-loader"),
+//             options: {
+//               ident: "postcss",
+//               plugins: () => [
+//                 require("postcss-flexbugs-fixes"),
+//                 autoprefixer({
+//                   browsers: [
+//                     ">1%",
+//                     "last 4 versions",
+//                     "Firefox ESR",
+//                     "not ie < 9" // React doesn't support IE8 anyway
+//                   ],
+//                   flexbox: "no-2009"
+//                 })
+//               ]
+//             }
+//           }
+//         ]
+//       },
+//       {
+//         test: /\.scss$/,
+//         // include: config.paths.appRoot,
+//         use: [
+//           require.resolve("style-loader"),
+//           {
+//             loader: require.resolve("css-loader"),
+//             options: {}
+//           },
+//           {
+//             loader: require.resolve("postcss-loader"),
+//             options: {
+//               ident: "postcss",
+//               plugins: () => [
+//                 require("postcss-flexbugs-fixes"),
+//                 autoprefixer({
+//                   browsers: [
+//                     ">1%",
+//                     "last 4 versions",
+//                     "Firefox ESR",
+//                     "not ie < 9" // React doesn't support IE8 anyway
+//                   ],
+//                   flexbox: "no-2009"
+//                 })
+//               ]
+//             }
+//           },
+//           {
+//             loader: require.resolve("sass-loader"),
+//             options: {}
+//           }
+//         ]
+//       },
+
+//       {
+//         test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+//         loader: 'file-loader?name=fonts/[name].[ext]'
+//       },
+
+//       {
+//         exclude: [/\.js$/, /\.jsx$/, /\.html$/, /\.json$/, /\.scss$/, /\.css$/, /\.ttf$/],
+//         loader: require.resolve('file-loader'),
+//         options: {
+//           name: 'static/media/[name].[hash:8].[ext]',
+//         },
+//       }
+//     ]
+//   }
+// }
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,25 +1,46 @@
-const path = require('path');
+process.env.NODE_ENV = "production";
+const path = require("path");
 const config = require("../config");
-const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const extractCSS = new ExtractTextPlugin('code/app.css');
+const extractSASS = new ExtractTextPlugin("code/app.css");
 
+const autoprefixer = require("autoprefixer");
+const extractCSS = new ExtractTextPlugin("code/app.css");
 
 module.exports = {
-  // GENERAL STUFF
-  target: 'web',
-  devtool: 'source-map',
+  mode: "production",
+
+  // IN/OUT files
+  entry: [require.resolve("babel-polyfill"), config.paths.webRoot],
+  output: {
+    path: config.paths.deployWwwRoot,
+    filename: "code/bundle.js"
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx"],
+    modules: ["node_modules", "bower_components"],
+    descriptionFiles: ["package.json", "bower.json"],
+    alias: {
+      BowerComponents: path.resolve(config.paths.appRoot + "/bower_components"),
+      NodeModules: path.resolve(config.paths.appRoot + "/node_modules"),
+      AppRoot: path.resolve(config.paths.appRoot)
+    }
+  },
+  devtool: "source-map",
+  target: "web",
+
+  // PLUGINS
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
       }
     }),
     new HtmlWebpackPlugin({
-      template: 'www/index.html',
+      template: "www/index.html",
       minify: {
         removeComments: true,
         collapseWhitespace: true,
@@ -30,50 +51,25 @@ module.exports = {
         keepClosingSlash: true,
         minifyJS: true,
         minifyCSS: true,
-        minifyURLs: true,
+        minifyURLs: true
       },
       inject: false
     }),
-    new UglifyJSPlugin({
-      sourceMap: true
-    }),
-    extractCSS,
+    extractCSS
   ],
-  resolve: {
-    extensions: ['.js', '.jsx'],
-    modules: ['node_modules', 'bower_components'],
-    descriptionFiles: ['package.json', 'bower.json'],
-    alias: {
-      BowerComponents: path.resolve(config.paths.appRoot + "/bower_components"),
-      NodeModules: path.resolve(config.paths.appRoot + "/node_modules"),
-      AppRoot: path.resolve(config.paths.appRoot)
-    }
-  },
-  node: {
-    fs: 'empty'
-  },
-
-
-  // IN/OUT files
-  entry: [
-    require.resolve('babel-polyfill'),
-    config.paths.webRoot
-  ],
-  output: {
-    path: config.paths.deployWwwRoot,
-    filename: 'code/bundle.js'
-  },
-
 
   // LOADERS
   module: {
-    rules: [ //
+    rules: [
+      //
       {
         test: /\.html$/,
-        use: [{
-          loader: require.resolve("html-loader"),
-          options: { minimize: true }
-        }]
+        use: [
+          {
+            loader: require.resolve("html-loader"),
+            options: { minimize: true }
+          }
+        ]
       },
       {
         test: /\.(js|jsx)$/,
@@ -84,7 +80,7 @@ module.exports = {
           presets: [
             require("babel-preset-env"),
             require("babel-preset-react"),
-            require("babel-preset-stage-2"),
+            require("babel-preset-stage-2")
           ]
         }
       },
@@ -93,34 +89,35 @@ module.exports = {
         // include: config.paths.appRoot,
         loader: extractCSS.extract(
           Object.assign({
-            fallback: require.resolve('style-loader'),
-            use: [{
-                loader: require.resolve('css-loader'),
+            fallback: require.resolve("style-loader"),
+            use: [
+              {
+                loader: require.resolve("css-loader"),
                 options: {
                   importLoaders: 1,
                   minimize: true,
-                  sourceMap: true,
-                },
+                  sourceMap: true
+                }
               },
               {
-                loader: require.resolve('postcss-loader'),
+                loader: require.resolve("postcss-loader"),
                 options: {
-                  ident: 'postcss',
+                  ident: "postcss",
                   plugins: () => [
-                    require('postcss-flexbugs-fixes'),
+                    require("postcss-flexbugs-fixes"),
                     autoprefixer({
                       browsers: [
-                        '>1%',
-                        'last 4 versions',
-                        'Firefox ESR',
-                        'not ie < 9', // React doesn't support IE8 anyway
+                        ">1%",
+                        "last 4 versions",
+                        "Firefox ESR",
+                        "not ie < 9" // React doesn't support IE8 anyway
                       ],
-                      flexbox: 'no-2009',
-                    }),
-                  ],
-                },
+                      flexbox: "no-2009"
+                    })
+                  ]
+                }
               }
-            ],
+            ]
           })
         )
       },
@@ -129,65 +126,63 @@ module.exports = {
         // include: config.paths.appRoot,
         loader: extractCSS.extract(
           Object.assign({
-            fallback: require.resolve('style-loader'),
-            use: [{
-                loader: require.resolve('css-loader'),
+            fallback: require.resolve("style-loader"),
+            use: [
+              {
+                loader: require.resolve("css-loader"),
                 options: {
                   importLoaders: 1,
                   minimize: true,
-                  sourceMap: true,
-                },
+                  sourceMap: true
+                }
               },
               {
-                loader: require.resolve('postcss-loader'),
+                loader: require.resolve("postcss-loader"),
                 options: {
-                  ident: 'postcss',
+                  ident: "postcss",
                   plugins: () => [
-                    require('postcss-flexbugs-fixes'),
+                    require("postcss-flexbugs-fixes"),
                     autoprefixer({
                       browsers: [
-                        '>1%',
-                        'last 4 versions',
-                        'Firefox ESR',
-                        'not ie < 9', // React doesn't support IE8 anyway
+                        ">1%",
+                        "last 4 versions",
+                        "Firefox ESR",
+                        "not ie < 9" // React doesn't support IE8 anyway
                       ],
-                      flexbox: 'no-2009',
-                    }),
-                  ],
-                },
+                      flexbox: "no-2009"
+                    })
+                  ]
+                }
               },
               {
-                loader: require.resolve('sass-loader'),
-              },
-            ],
+                loader: require.resolve("sass-loader")
+              }
+            ]
           })
         )
       },
 
       {
         test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: 'file-loader?name=fonts/[name].[ext]'
+        loader: "file-loader?name=fonts/[name].[ext]"
       },
 
-
       {
-        exclude: [/\.js$/, /\.jsx$/, /\.html$/, /\.json$/, /\.scss$/, /\.css$/, /\.ttf$/],
-        loader: require.resolve('file-loader'),
+        exclude: [
+          /\.js$/,
+          /\.jsx$/,
+          /\.html$/,
+          /\.json$/,
+          /\.scss$/,
+          /\.css$/,
+          /\.ttf$/
+        ],
+        loader: require.resolve("file-loader"),
         options: {
-          name: 'static/media/[name].[hash:8].[ext]',
-        },
+          name: "static/media/[name].[hash:8].[ext]"
+        }
       }
     ]
-  },
-
-
-  // Some libraries import Node modules but don't use them in the browser.
-  // Tell Webpack to provide empty mocks for them so importing them works.
-  node: {
-    dgram: 'empty',
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-    child_process: 'empty',
   }
-}
+  //
+};
